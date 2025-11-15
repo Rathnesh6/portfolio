5e6f27a8c25b288af2d1b2eb533faa422a200dbd
rathnesh28 <rathneshinfouna@gmail.com>
2025-11-15 21:57:08 +0530

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// pages/_app.tsx
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
